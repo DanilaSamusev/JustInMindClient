69bb5d4f39f352e771faa3f723d52fd3392288a9
Aldamar <danila.samusev@mail.ru>
2019-09-18 20:29:04 +0300

    Tiicket table has got css.

--- src/Components/TicketTable.js
+++ src/Components/TicketTable.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/ticketsTable.css'
 
 export default class TicketTable extends React.Component {
 
@@ -39,38 +40,39 @@ export default class TicketTable extends React.Component {
 
     render() {
 
-        if (this.state.tickets == null){
+        if (this.state.tickets == null) {
             return (
                 <div>No Data</div>
             )
         }
 
         return (
+            <div className='ticketsTable'>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Desired Date</th>
+                        <th>Urgency</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
 
-            <table>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Desired Date</th>
-                    <th>Urgency</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                </tr>
-
-                {
-                    this.state.tickets.map(ticket =>
-                        <tr>
-                            <td>{ticket.id}</td>
-                            <td>{ticket.name}</td>
-                            <td>{ticket.desired_Resolution_Date}</td>
-                            <td>{ticket.urgency_Id}</td>
-                            <td>{ticket.state_Id}</td>
-                            <td>
-                                <button>Action</button>
-                            </td>
-                        </tr>
-                )}
-            </table>
+                    {
+                        this.state.tickets.map(ticket =>
+                            <tr>
+                                <td>{ticket.id}</td>
+                                <td>{ticket.name}</td>
+                                <td>{ticket.desired_Resolution_Date}</td>
+                                <td>{ticket.urgency_Id}</td>
+                                <td>{ticket.state_Id}</td>
+                                <td>
+                                    <button>Action</button>
+                                </td>
+                            </tr>
+                        )}
+                </table>
+            </div>
         )
     }
 }
